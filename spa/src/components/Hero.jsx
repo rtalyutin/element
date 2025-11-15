0828f560852042af95beb33703182065ba8c9936
rtalyutin <88474835+rtalyutin@users.noreply.github.com>
2025-11-15 18:42:04 +0300

    Update Hero.jsx

--- spa/src/components/Hero.jsx
+++ spa/src/components/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => (
     </div>
     <div className="hero__visual" role="presentation" aria-hidden="true">
       <iframe
-        src="https://youtu.be/o7GPNI3Okb0?autoplay=1&mute=1&loop=1&controls=0&playlist=6pvhLJ3d0Xk"
+        src="https://s3.twcstorage.ru/e9dc5ea4-6dc9267d-85ca-4ae9-a41f-2895e9542a04/video_2025-11-15_18-06-54.mp4"
         title="Видео процесса кузовного ремонта"
         allow="autoplay; fullscreen"
         allowFullScreen
