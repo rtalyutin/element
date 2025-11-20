7fc0d1509483509698c3f3ca2913eb3044cccd86
rtalyutin <88474835+rtalyutin@users.noreply.github.com>
2025-11-20 12:26:49 +0300

    Update gallerySlides.js

--- spa/src/data/gallerySlides.js
+++ spa/src/data/gallerySlides.js
@@ -1,6 +1,21 @@
-import auto01 from '../assets/gallery/auto-01.svg';
-import auto02 from '../assets/gallery/auto-02.svg';
-import auto03 from '../assets/gallery/auto-03.svg';
+import auto1 from '../assets/gallery/auto1.jpg';
+import auto2 from '../assets/gallery/auto2.jpg';
+import auto3 from '../assets/gallery/auto3.jpg';
+import auto4 from '../assets/gallery/auto4.jpg';
+import auto5 from '../assets/gallery/auto5.jpg';
+import auto6 from '../assets/gallery/auto6.jpg';
+import auto7 from '../assets/gallery/auto7.jpg';
+import auto8 from '../assets/gallery/auto8.jpg';
+import auto9 from '../assets/gallery/auto9.jpg';
+import auto10 from '../assets/gallery/auto10.jpg';
+import auto11 from '../assets/gallery/auto11.jpg';
+import auto12 from '../assets/gallery/auto12.jpg';
+import auto13 from '../assets/gallery/auto13.jpg';
+import auto14 from '../assets/gallery/auto14.jpg';
+import auto15 from '../assets/gallery/auto15.jpg';
+import auto16 from '../assets/gallery/auto16.jpg';
+import auto17 from '../assets/gallery/auto17.jpg';
+
 import arches01 from '../assets/gallery/arches-01.svg';
 import arches02 from '../assets/gallery/arches-02.svg';
 import arches03 from '../assets/gallery/arches-03.svg';
@@ -17,20 +32,96 @@ const gallerySlides = {
       id: 'auto-1',
       title: 'Подготовка поверхности',
       caption: 'Шлифуем кузов и размечаем проблемные зоны перед покраской.',
-      image: auto01,
+      image: auto1,
     },
     {
       id: 'auto-2',
       title: 'Подбор оттенка',
       caption: 'Настраиваем факел покраски и тестируем покрытие на стенде.',
-      image: auto02,
+      image: auto2,
     },
     {
       id: 'auto-3',
       title: 'Финишное нанесение',
       caption: 'Контролируем равномерность слоя и температуру в камере.',
-      image: auto03,
+      image: auto3,
     },
+    {
+      id: 'auto-4',
+      title: 'Финишное нанесение',
+      caption: '',
+      image: auto4,
+    },
+    {
+      id: 'auto-5',
+      title: 'Финишное нанесение',
+      caption: '',
+      image: auto5,
+    },
+    {
+      id: 'auto-6',
+      title: 'Финишное нанесение',
+      caption: '',
+      image: auto6,
+    },
+    {
+      id: 'auto-7',
+      title: 'Финишное нанесение',
+      caption: '',
+      image: auto7,
+    },
+    {
+      id: 'auto-8',
+      title: 'Финишное нанесение',
+      caption: '',
+      image: auto8,
+    },
+    {
+      id: 'auto-9',
+      title: 'Финишное нанесение',
+      caption: '',
+      image: auto9,
+    },{
+      id: 'auto-10',
+      title: 'Подготовка поверхности',
+      caption: 'Шлифуем кузов и размечаем проблемные зоны перед покраской.',
+      image: auto10,
+    },{
+      id: 'auto-11',
+      title: 'Подготовка поверхности',
+      caption: 'Шлифуем кузов и размечаем проблемные зоны перед покраской.',
+      image: auto11,
+    },{
+      id: 'auto-12',
+      title: 'Подготовка поверхности',
+      caption: 'Шлифуем кузов и размечаем проблемные зоны перед покраской.',
+      image: auto12,
+    },{
+      id: 'auto-13',
+      title: 'Подготовка поверхности',
+      caption: 'Шлифуем кузов и размечаем проблемные зоны перед покраской.',
+      image: auto1,
+    },{
+      id: 'auto-14',
+      title: 'Подготовка поверхности',
+      caption: 'Шлифуем кузов и размечаем проблемные зоны перед покраской.',
+      image: auto14,
+    },{
+      id: 'auto-15',
+      title: 'Подготовка поверхности',
+      caption: 'Шлифуем кузов и размечаем проблемные зоны перед покраской.',
+      image: auto15,
+    },{
+      id: 'auto-16',
+      title: 'Подготовка поверхности',
+      caption: 'Шлифуем кузов и размечаем проблемные зоны перед покраской.',
+      image: auto16,
+    },{
+      id: 'auto-17',
+      title: 'Подготовка поверхности',
+      caption: 'Шлифуем кузов и размечаем проблемные зоны перед покраской.',
+      image: auto1,
+    }
   ],
   arches: [
     {
